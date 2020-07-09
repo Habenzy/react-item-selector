146d783fc763de8cfc2af53e25d54d088be2ff53
Robert Stauss <robertrstauss@gmail.com>
2020-07-09 10:06:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      postsData: [],
+      selectedPost: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then((postCollection) => {
+        this.setState({ postsData: postCollection })
+      })
+  }
+
+  selectPost = (post) => {
+
+    this.setState({ selectedPost: post })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <PostList posts={this.state.postsData} select={this.selectPost} />
+        <PostDisplay post={this.state.selectedPost} />
+      </div>
+    )
+  }
+}
+
+function PostList(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {props.posts.map((post, index) => {
+        return <li
+          key={index}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.select(post)
+          }
+          }>
+          {post.title}
+        </li>
+      })}
+    </ul>
+  )
+}
+
+function PostDisplay(props) {
+  if (props.post) {
+    return (
+      <div id='container'>
+        Title: {props.post.title}
+        <br />
+        Content: {props.post.body}
+      </div>
+    )
+  } else {
+    return <div id='container'>No Post Selected</div>
+  }
 }
 
 export default App;
